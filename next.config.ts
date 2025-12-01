b7c5353db611ea34af60e48475adcab8e1eaa281
kimura.coki <kimura.coki@gmail.com>
2025-12-01 17:27:42 +0900

    Add deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
+// リポジトリ名に合わせて変更してください
+const repoName = "life-game";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+ output: "export",
+  // 画像を使う予定があるなら unoptimized を true にしておくと楽です
+  images: {
+    unoptimized: true
+  },
+  // yourname.github.io/life-game でホストする場合
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : ""
 };
 
 export default nextConfig;
